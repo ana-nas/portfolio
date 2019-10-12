b331855f7d267fe930e03ddf084232b57cbc3e29
Anastasia Konovalova <akonovalova@ozon.ru>
2019-10-12 17:36:39 +0300

    added tabs

---
+++ components/Tabs.vue
@@ -0,0 +1,108 @@
+<script>
+export default {
+  name: 'Tabs',
+	props: {
+		initial: {
+			type: String,
+			required: true,
+		},
+		tabs: {
+			type: Array,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			active: this.initial,
+		};
+	},
+	computed: {
+		tabDetailsName() {
+			return `tab-details-${this.active}`;
+		},
+	},
+	methods: {
+		selectTab(name) {
+			if (name) {
+				this.active = name;
+			}
+		},
+		tabSwitcherName(name) {
+			return `tab-switcher-${name}`;
+		},
+	},
+};
+</script>
+
+<template>
+	<div class="wrapper">
+		<div class="content">
+			<slot :name="tabDetailsName" />
+		</div>
+
+    <ul class="switcher">
+				<li
+					v-for="(tab, index) in tabs"
+					:key="index"
+					:class="['tab', {'active': active === tab}]"
+					@click="selectTab(tab)"
+				>
+					<slot :name="tabSwitcherName(tab)">
+						{{ tab }}
+					</slot>
+				</li>
+		  </ul>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 20vh;
+}
+.tab {
+  color: $c-dark;
+  cursor: pointer;
+  margin-right: 5rem;
+}
+.content {
+  width: 100%;
+  position: relative;
+  padding: 0 5rem;
+  margin-bottom: 5rem;
+  h2 {
+    font-size: 36px;
+    font-weight: bold;
+    margin-bottom: 2rem;
+  }
+  p {
+    max-width: 300px;
+  }
+  & img {
+    height: 240px;
+    position: absolute;
+    right: 0;
+    top: -2rem;
+  }
+}
+.switcher {
+	padding: 0;
+  list-style: none;
+	display: flex;
+  align-items: center;
+	justify-content: space-between;
+  max-width: 300px;
+  margin-bottom: 3rem;
+}
+.item {
+	display: block;
+}
+.active {
+	color: $c-accent;
+  font-weight: bold;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,87 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        portfolio 1
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project KEK
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div class="grid">
+    <section class="about">
+      <div class="about-text">
+        <h2 class="title">
+          Ana Konovalova
+        </h2>
+        <div class="description">
+          <p>
+            I’m a digital product designer, working in e-commerce project
+            <a href="https://www.ozon.ru/">Ozon.ru</a> and developing financial services
+            <a href="https://invest.ozon.ru/">Ozon.Invest</a>,
+            <a href="https://card.ozon.ru">Ozon.Wallet</a>,
+            payments and credit systems.
+            </p>
+            <p>
+            I trying to create senses in my life,
+            so you can read my principles of work.
+            </p>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+    <section class=portfolio>
+      <Tabs
+        :initial="'ozon'"
+        :tabs="tabs"
+      >
+        <!-- ozon -->
+        <template slot="tab-switcher-ozon">
+          <p class="switcher-item">ozon</p>
+        </template>
+        <template slot="tab-details-ozon">
+          <h2>Ozon.Invest</h2>
+          <p>Text about my projects and technologies I'm using here.</p>
+          <p>Here is a <a href="#">link</a> to the project I am working on.</p>
+          <img src="/assets/img/ozon.jpg" alt="ozon">
+        </template>
+
+        <!-- credit -->
+        <template slot="tab-switcher-credit">
+          <p class="switcher-item">credit services</p>
+        </template>
+        <template slot="tab-details-credit">
+          <h2>Credit services</h2>
+          <p>Text about my projects and technologies I'm using here.</p>
+          <img src="/assets/img/credit.jpg" alt="credit">
+        </template>
+
+        <!-- apps -->
+        <template slot="tab-switcher-apps">
+          <p class="switcher-item">apps</p>
+        </template>
+        <template slot="tab-details-apps">
+          <h2>Apps</h2>
+          <p>Text about my projects and technologies I'm using here.</p>
+          <img src="/assets/img/apps.jpg" alt="apps">
+        </template>
+      </Tabs>
+    </section>
+  </div>
 </template>
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
 
+<script>
+import Tabs from '/components/Tabs.vue'
 export default {
   components: {
-    AppLogo
-  }
+    Tabs
+  },
+  data() {
+    return {
+      	tabs: [
+          'ozon',
+          'credit',
+          'apps',
+        ],
+    };
+  },
 }
 </script>
 
 <style>
+
+
 .container {
   min-height: 100vh;
   display: flex;
