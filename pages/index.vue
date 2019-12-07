a18ef7dcbc0c568d8ce1332687baa537379a54c7
Anastasia Konovalova <akonovalova@ozon.ru>
2019-12-08 00:48:57 +0300

    new layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,142 +1,82 @@
 <template>
   <div class="grid">
     <section class="about">
-      <div class="about-text">
+      <div class="vertical_text">
+        <p> Ana Konovalova
+        </p>
+        </div>
+    	<div class="about-text">
         <h2 class="title">
-          Ana Konovalova
+        I’m a digital product designer with an interest in user research and development.
         </h2>
         <div class="description">
           <p>
-            I’m a digital product designer, working in e-commerce project
-            <a href="https://www.ozon.ru/">Ozon.ru</a> and developing financial services
-            <a href="https://invest.ozon.ru/">Ozon.Invest</a>,
-            <a href="https://card.ozon.ru">Ozon.Wallet</a>,
-            payments and credit systems.
-            </p>
-            <p>
-            I trying to create senses in my life,
-            so you can read my principles of work.
-            </p>
+          Currently work at <a href="https://www.ozon.ru/">Ozon</a> (Moscow, Russia).
+          Mostly developing financial services inside the e-commerce product.
+          Passionate about creating senses in my life, so you can read my principles of work.
+          I like to try new things and meet new people, so drop me
+          an email or say hi on Telegram.
+          </p>
+          <div class="projects">
+            <ul class="list">
+              <li><a href="https://www.ozon.ru/"> Ozon Invest </a></li>
+              <li><a href="https://www.ozon.ru/"> Credit services </a></li>
+              <li><a href="https://www.ozon.ru/"> Ozon Card </a></li>
+              <li><a href="https://www.ozon.ru/"> Infographics </a></li>
+           </ul>
+          </div>
         </div>
-      </div>
-    </section>
-    <section class=portfolio>
-      <Tabs
-        :initial="'ozon'"
-        :tabs="tabs"
-      >
-        <!-- ozon -->
-        <template slot="tab-switcher-ozon">
-          <p class="switcher-item">ozon</p>
-        </template>
-        <template slot="tab-details-ozon">
-          <h1>Ozon.Invest</h1>
-          <p>Text about my projects and technologies I'm using here.</p>
-          <p>Here is a <a href="#">link</a> to the project I am working on.</p>
-          <img src="/assets/img/ozon.jpg" alt="ozon">
-        </template>
-
-        <!-- credit -->
-        <template slot="tab-switcher-credit">
-          <p class="switcher-item">credit services</p>
-        </template>
-        <template slot="tab-details-credit">
-          <h1>Credit services</h1>
-          <p>Text about my projects and technologies I'm using here.</p>
-          <img src="/assets/img/credit.jpg" alt="credit">
-        </template>
-
-        <!-- apps -->
-        <template slot="tab-switcher-apps">
-          <p class="switcher-item">apps</p>
-        </template>
-        <template slot="tab-details-apps">
-          <h1>Apps</h1>
-          <p>Text about my projects and technologies I'm using here.</p>
-          <img src="/assets/img/apps.jpg" alt="apps">
-        </template>
-      </Tabs>
-    </section>
-    <section class=footer>
-      <div class=contacts>
-        <p>I like to try new things and meet new people,
-          so drop me an email or say hi on Telegram.
-        </p>
-      </div>
-      <div class="social">
-          <ul class="list">
+			</div>
+	  </section>
+    <section class="photo">
+      <div class=social>
+        <ul class="social_list">
+            <li><a href="#"><img src="~/assets/img/behance.svg" alt="behance"></a></li>
+            <li><a href="#"><img src="~/assets/img/linkedin.svg" alt="linkedin"></a></li>
             <li><a href="#"><img src="~/assets/img/facebook.svg" alt="facebook"></a></li>
             <li><a href="#"><img src="~/assets/img/instagram.svg" alt="instagram"></a></li>
-            <li><a href="#"><img src="~/assets/img/linkedin.svg" alt="linkedin"></a></li>
-            <li><a href="#"><img src="~/assets/img/behance.svg" alt="behance"></a></li>
+
+
           </ul>
+      </div>
+      <div class=bg>
+        <a href="#"><img src="~/assets/img/rectangle.png"></a>
       </div>
     </section>
   </div>
 </template>
 
 
-<script>
-import Tabs from '~/components/Tabs.vue'
-export default {
-  components: {
-    Tabs
-  },
-  data() {
-    return {
-      	tabs: [
-          'ozon',
-          'credit',
-          'apps',
-        ],
-    };
-  },
-}
-</script>
 
 <style lang="scss" scoped>
 .grid {
   height: 100%;
   display: grid;
-  grid-template-areas: "about portfolio"
-                       "footer footer";
-  grid-template-columns: 3fr 4fr;
-  grid-template-rows: 2fr 1fr;
-  background-color: #1C1C1C;
-    &-grid-footer {
-      display: grid;
-      grid-template-areas: "footer social";
-      grid-template-columns: 3fr 4fr;
-    }
-    &-grid-social {
-      display: grid;
-      grid-template-areas: "empty list";
-      grid-template-columns: 4fr 4fr;
-      background-color: $c-bg;
+  grid-template-areas: "about photo";
+  grid-template-columns: 7fr 5fr;
+}
+
+.vertical_text{
+  p{
+    transform: rotate(-90deg);
+    position: absolute;
+    left: 10px;
+    color: #CD0242;
+    font-family: Inconsolata;
   }
 }
 
 .about {
   grid-area: about;
-  border-right: 2px solid #fff;
   display: flex;
   justify-content: space-between;
   align-items: center;
   flex-direction: column;
   padding: 0 4rem;
-  padding-top: 20vh;
-  &-text,
-  &-social {
-    width: 360px;
-    height: 300px;
-  }
-  &-social {
-    width: 360px;
-    height: auto;
-    margin-bottom: 3rem;
-  }
+  padding-top: 10vh;
+
   .title {
-    color: #fff;
+    color: #162C83;
     font-weight: bold;
     margin-bottom: 2rem;
   }
@@ -145,58 +85,48 @@ export default {
       padding: 0;
       margin: 0;
       margin-bottom: 1rem;
-      color:#fff;
+      color:#02090F;
+    }
+    .list {
+      padding-top: 7rem;
+      padding-left: 0rem;
+      list-style: none;
+      display: flex;
+      justify-content: space-between;
+      li {
+        list-style-type: none;
+        display: inline-block;
+      }
+
+      a {
+        text-decoration: none;
+        color: #8D8C90;
+        font-family: Inconsolata;
+      }
     }
   }
 }
-.portfolio {
-  grid-area: portfolio;
-  height: 100%;
-    p {
-      padding: 0;
-      margin: 0;
-      margin-bottom: 1rem;
-      color:#fff;
+.photo{
+  .bg {
+    img{
+      grid-area: photo;
+      height: 100%;
     }
-    h1 {
-      padding: 0;
-      margin: 0;
-      margin-bottom: 1rem;
-      color:#fff;
-    }
-}
-
-.footer {
-  grid-area: footer;
-  display:flex;
-  align-items: center;
-  justify-content: space-between;
-  border-top: 2px solid #fff;
-  padding:0 4rem;
-  position: relative;
-  color: #fff;
-
-  .contacts {
   }
-  .list {
-    padding-right: 4rem;
-    list-style: none;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    li {
-      border-left: 2px solid #fff;
-      border-right: 2px solid #fff;
+  .social{
+    .social_list{
+      padding-top: 3rem;
+      padding-left: 20rem;
+      padding-right: 4rem;
+      padding-bottom: 3rem;
+      list-style: none;
+      display: flex;
+      justify-content: space-between;
+      li {
       list-style-type: none;
       display: inline-block;
-    }
-
-    a {
-      text-decoration: none;
-      color: #fff;
+      }
     }
   }
 }
-
-
 </style>
