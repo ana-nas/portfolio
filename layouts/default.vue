9710dddfc9c6344476d48c81babe502ea36e03d2
Nazym Jumadilova <ndzhumadilova@ozon.ru>
2020-02-17 13:16:07 +0300

    Update desktop design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,106 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+    <div class="wrapper">
+        <nuxt />
+        <div class="menu">
+            <ul class="menu__list">
+                <li class="menu__item">
+                    <nuxt-link :to="{name: 'index'}">
+                        <h5 :class="['menu__title', {'menu__title--active': $route.name === 'index'}]">About</h5>
+					</nuxt-link>
+                </li>
+                <li class="menu__item">
+                    <nuxt-link :to="{name: 'p2b'}">
+                        <h5 :class="['menu__title', {'menu__title--active': $route.name === 'p2b'}]">Ozon Invest</h5>
+                        <p class="menu__subtitle">p2b platform</p>
+                    </nuxt-link>
+                </li>
+                <li class="menu__item">
+                    <nuxt-link :to="{name: 'p2b'}">
+                        <h5 :class="['menu__title', {'menu__title--active': $route.name === 'credit'}]">Ozon Credit service</h5>
+                        <p class="menu__subtitle">description</p>
+					</nuxt-link>
+                </li>
+                <li class="menu__item">
+                    <nuxt-link :to="{name: 'p2b'}">
+                        <h5 :class="['menu__title', {'menu__title--active': $route.name === 'finance'}]">Ozon Financial service</h5>
+                        <p class="menu__subtitle">description</p>
+					</nuxt-link>
+                </li>
+                <li class="menu__item">
+                    <nuxt-link :to="{name: 'p2b'}">
+                        <h5 :class="['menu__title', {'menu__title--active': $route.name === 'editorial'}]">Editorial</h5>
+					</nuxt-link>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
-<style>
-html {
-  font-family: "Gotham Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<style lang="scss" scoped>
+.menu {
+	min-width: 300px;
+	background-color: $c-bg;
+    position: fixed;
+    padding: 0;
+    right: 0;
+    bottom: 0;
+	top: 0;
+
+	@include xxl {
+		min-width: 500px;
+	}
+
+    &__list {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+		flex-direction: column;
+		height: 100%;
+		justify-content: space-around;
+		border-left: 2px solid $c-primary;
+    }
+
+    &__item {
+		height: 100%;
+		justify-content: center;
+		flex-direction: column;
+		border-bottom: 2px solid $c-primary;
+		padding: 56px;
+
+		&:last-child {
+			border-bottom: none;
+		}
+
+		& a {
+			text-decoration: none;
+			color: $c-primary;
+		}
+    }
+
+    &__title {
+		font-size: $size-l;
+		font-weight: $semibold;
+
+		&:hover {
+			color: $c-accent;
+		}
+
+        &--active {
+			color: $c-accent;
+
+			&:hover {
+				color: $c-primary;
+			}
+        }
+    }
+
+    &__subtitle {
+		font-family: 'Inconsolata', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		font-size: $size-s;
+		color: $c-secondary;
+		margin-top: 12px;
+    }
 }
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,189 +1,165 @@
+<script>
+export default {
+	data() {
+		return {
+			footerLinks: [
+				{
+					title: 'Behance',
+					link: 'https://www.behance.net/anakonovalova',
+				},
+				{
+					title: 'Linkedin',
+					link: 'https://www.linkedin.com/in/anastasia-konovalova-27b001118/',
+				},
+				{
+					title: 'Facebook',
+					link: 'https://www.facebook.com/betaniyaho',
+				},
+				{
+					title: 'Instagram',
+					link: 'https://www.instagram.com/cosmic_ananas',
+				},
+			]
+		}
+	},
+};
+</script>
 <template>
-  <div class="grid">
-    <section class="about">
-      <div class=social>
-        <ul class="social_list">
-            <!--<li><a href="#"><img src="~/assets/img/behance.svg" alt="behance"></a></li>
-            <li><a href="#"><img src="~/assets/img/linkedin.svg" alt="linkedin"></a></li>
-            <li><a href="#"><img src="~/assets/img/facebook.svg" alt="facebook"></a></li>
-            <li><a href="#"><img src="~/assets/img/instagram.svg" alt="instagram"></a></li>-->
-            <li><a class="about-link-social" href="https://medium.com/@ana_nas" target="_blank"> Medium </a></li>
-            <li><a class="about-link-social" href="https://www.linkedin.com/in/anastasia-konovalova-27b001118/" target="_blank"> Linkedin </a></li>
-            <li><a class="about-link-social" href="https://www.behance.net/anakonovalova" target="_blank"> Behance </a></li>
-            <li><a class="about-link-social" href="https://www.instagram.com/cosmic_ananas" target="_blank"> Instagram </a></li>
-            <li><a class="about-link-social" href="https://www.facebook.com/betaniyaho" target="_blank"> Facebook </a></li>
-         </ul>
-      </div>
-      <div class="vertical_text">
-        <p> Ana Konovalova
-        </p>
+    <div class="content">
+        <div class="row wrapper">
+            <div class="col-12 col-md-6 col-lg-7 col-xl-8 spread">
+				<div class="row">
+					<div class="col">
+						 <h1 class="name">Ana Konovalova</h1>
+					</div>
+				</div>
+                <div class="row">
+                    <div class="col">
+                        <h2 class="title spacing">
+                           I&rsquo;m a&nbsp;digital product designer with an&nbsp;interest in&nbsp;user research and development.
+                        </h2>
+                        <p class="description">
+                            Currently I&nbsp;work&nbsp;at&nbsp;<a target="_blank" class="link" href="#">Ozon</a> (Moscow, Russia).
+                            Mostly developing financial services inside the&nbsp;e-commerce corporation.
+							I&nbsp;try to&nbsp;create senses in&nbsp;my&nbsp;life, so&nbsp;you can read&nbsp;my&nbsp;<a target="_blank" class="link" href="#">principles of work</a>.
+							I&nbsp;like to&nbsp;try new things and meet new people, so&nbsp;drop me&nbsp;an&nbsp;email or&nbsp;say hi&nbsp;on&nbsp;Telegram.
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <ul class="footer">
+                            <li v-for="(item, i) in footerLinks" :key="i">
+                                <a
+                                    class="footer__link"
+                                    :href="item.link"
+                                    target="_blank"
+                                >{{ item.title }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-    	<div class="about-text">
-        <h2 class="title">
-        I’m a digital product designer with an interest in user psychology and development.
-        </h2>
-        <div class="description">
-          <p>
-          Currently, I work for <a class="about-link" href="https://www.invest.ozon.ru/ " target="_blank">Ozon Invest</a> (Moscow, Russia).
-          which is an investment platform helping small businesses expand and I also,
-		      do developing financial services inside the e-commerce product <a class="about-link" href="https://www.ozon.ru/" target="_blank">Ozon.</a>
-		      I strive to create lasting connections between the person and the product
-		      through meaningful design.
-		      I passionate about creating senses in my life, so you can read my
-          <a class="about-link" href="https://medium.com/@ana_nas/principles-for-designing-products-25d81ab3c459" target="_blank">
-          principles of work.</a>
-          I like to try new things and meet new people, so drop me
-          an email or say hi on Telegram.
-          </p>
-          <div class="projects">
-			      <h3 class="title">
-			      	Projects
-			      </h3>
-            <ul class="list">
-              <li><a class="about-link" href="https://invest.ozon.ru/investor" target="_blank"> Invest (for investors)</a></li>
-              <li><a class="about-link" href="https://www.invest.ozon.ru/" target="_blank"> Invest (for borrowers) </a></li>
-              <li><a class="about-link" href="https://www.ozon.ru/" target="_blank"> Ozon </a></li>
-              <li><a class="about-link" href="https://card.ozon.ru/" target="_blank"> Ozon Card </a></li>
-           </ul>
-          </div>
-        </div>
-			</div>
-	  </section>
-    <section class="photo">
-      <div class="pic-wrapper">
-        <div />
-        <img src="~/assets/img/ana_konovalova.png">
-      </div>
-    </section>
-  </div>
+    </div>
 </template>
 
-
-
 <style lang="scss" scoped>
-.grid {
-  height: 100%;
-  display: grid;
-  grid-template-areas: "about photo";
-  grid-template-columns: 7fr 5fr;
+.content {
+	color: $c-text;
+	font-weight: $regular;
+	position: relative;
+    width: 100%;
+	height: 100%;
+
+	padding: 56px 80px;
 }
 
-.vertical_text{
-  p{
-    //transform: rotate(-90deg);
-    position: absolute;
-    left: 10px;
-    color: #CD0242;
-    font-family: Inconsolata;
-    padding:0 4rem;
-    //@include md{
-      //transform:rotate(0deg);
-    }
+.spread {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 }
 
-.about {
-  grid-area: about;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  padding: 0 4rem;
-  padding-top: 10vh;
+.name {
+	color: $c-accent;
+	font-size: $size-l;
+	font-weight: $semibold;
+}
 
-  .social{
-    position: absolute;
-    right:4rem;
-    top: 1rem;
-    .social_list{
-      padding-top: 3rem;
-      padding-left: 20rem;
-      padding-bottom: 3rem;
-      list-style: none;
-      display: flex;
-      justify-content: space-between;
-      li {
-      list-style-type: none;
-      display: inline-block;
-      margin-left: 2rem;
-      }
-    }
-  }
+.title {
+	line-height: 1.1;
+	color: $c-primary;
+	font-size: $size-xl;
+	font-weight: $bold;
+}
 
-  .about-link{
-    color:#162C83;
+.description {
+	line-height: 1.4;
+	font-size: $size-m;
+}
+
+.link {
+	color: $c-text;
     text-decoration: none;
-    border-bottom: 1px solid #162C83;
-  }
+	border-bottom: 2px solid $c-primary;
 
-    .about-link-social{
-    color:#8D8C90;
-    text-decoration: none;
-    //border-bottom: 1px solid #162C83;
-    font-family: Inconsolata;
+	&:hover {
+		border: none;
+	}
+}
 
-  }
+.spacing {
+	margin-bottom: 2rem;
+}
 
-  .title {
-    color: #162C83;
-    font-weight: bold;
-    margin-bottom: 2rem;
-  }
-  .description {
-    p {
-      padding: 0;
-      margin: 0;
-      margin-bottom: 1rem;
-      color:#02090F;
-    }
+.footer {
+	margin: 0;
+	padding: 0;
+    list-style: none;
 
-    h3 {
-      padding:0;
-      padding: 0;
-      margin: 0;
-      margin-top: 4.5rem;
-
-
-    }
-    .list {
-      padding-top: 2rem;
-      padding-left: 0rem;
-      list-style: none;
-      display: flex;
-      justify-content: space-between;
-      li {
+    & li {
         list-style-type: none;
         display: inline-block;
-      }
+		margin-right: 2.5rem;
 
-      a {
-        text-decoration: none;
-        //color: #8D8C90;
-        //font-family: Inconsolata;
-      }
+		&::last-child() {
+			margin-right: 0;
+		}
     }
-  }
+
+    &__link {
+		color: $c-secondary;
+		text-decoration: none;
+		font-family: "Inconsolata", 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
 }
-.photo{
-  padding-top: 8rem;
-  @include lg {
-    display: none;
-  }
 
-  .pic-wrapper {
-    position: relative;
-    div {
-      min-width: 574px;
-      height: 506px;
-      background-color: #EBEEF2;
+.menu {
+	position: fixed;
+	padding: 1rem;
+	right: 0;
+	bottom: 0;
+	top: 0;
+
+	&__list {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+	}
+
+    &__item {
     }
 
-    img{
-      grid-area: photo;
-      max-width: 279px;
-      position: absolute;
-      right: 4rem;
-      bottom: -2rem;
+    &__title {
+        &--active {
+			color: $c-primary;
+        }
     }
-  }
+
+    &__subtitle {
+    }
 }
 </style>
